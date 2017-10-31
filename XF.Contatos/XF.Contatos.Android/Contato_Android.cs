847cdd8b3f75f865ce886e636f7dc5b3ea82bf74
Eduardo <eduardo.dnunes@hotmail.com>
2017-10-30 22:43:56 -0200

    Início da implementação da Listagem de Contatos (Android)

---
+++ XF.Contatos/XF.Contatos.Android/Contato_Android.cs
@@ -0,0 +1,34 @@
+﻿using Android.Content;
+using Android.Net;
+using Android.Telephony;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Contacts;
+using XF.Contatos.Contato;
+using XF.Contatos.Models;
+
+[assembly: Dependency(typeof(Contato_Android))]
+namespace XF.Contatos.Droid
+{
+    class Contato_Android : IContato
+    {
+        public void GetAll()
+        {
+            List<Contato> contatos = new List<Contato>();
+
+            var book = new Xamarin.Contacts.AddressBook();
+            book.RequestPermission().ContinueWith(t => {
+                if (!t.Result)
+                {
+                    Console.WriteLine("Acesso negado à lista de contatos.");
+                    return;
+                }
+
+                foreach (Contact contact in book.OrderBy(c => c.LastName))
+                {
+                    contatos.Add(new Contato() { Nome = c.FirstName + " " + c.LastName})
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
