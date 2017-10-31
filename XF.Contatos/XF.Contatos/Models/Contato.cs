db3d495679ef82ac3093e6ae3708bb3ec27176f4
Eduardo <eduardo.dnunes@hotmail.com>
2017-10-30 22:36:31 -0200

    Add project files.

---
+++ XF.Contatos/XF.Contatos/Models/Contato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.Contatos.Models
+{
+    public class Contato
+    {
+        public Uri Imagem { get; set; }
+        public string Nome { get; set; }
+        public string Numero { get; set; }
+
+        public static List<Contato> GetAll()
+        {
+            return new List<Contato>();
+        }
+    }
+}
